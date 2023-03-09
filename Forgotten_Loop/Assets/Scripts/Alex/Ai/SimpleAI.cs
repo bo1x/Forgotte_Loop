1e9e2dc941e0bf50b7c3651562ee520269936233
Bo1x <alejandroboixperez@gmail.com>
2023-03-09 19:31:30 +0100

    fix simpleAI bug con find gameobject on drawgizmo

--- Forgotten_Loop/Assets/Scripts/Alex/Ai/SimpleAI.cs
+++ Forgotten_Loop/Assets/Scripts/Alex/Ai/SimpleAI.cs
@@ -9,11 +9,16 @@ public class SimpleAI : MonoBehaviour
     public bool DibujarEsfera = true;
     public bool perseguir = true;
     public float velocidadMovimiento = 5f;
-    
+
     // Start is called before the first frame update
-    void Start()
+
+    void Awake()
     {
         player = GameObject.Find("Player");
+    }
+    void Start()
+    {
+        
     }
 
     // Update is called once per frame
@@ -24,27 +29,27 @@ public class SimpleAI : MonoBehaviour
         //Debug.Log(Vector3.Distance(this.gameObject.transform.position, player.transform.position));
         if (Vector3.Distance(this.gameObject.transform.position, player.transform.position) < RangoVisionCircular && perseguir)
         {
-            this.transform.position = Vector3.MoveTowards(this.transform.position, player.transform.position, Time.deltaTime* velocidadMovimiento);
+            this.transform.position = Vector3.MoveTowards(this.transform.position, player.transform.position, Time.deltaTime * velocidadMovimiento);
         }
-
+      
 
     }
 
     void OnDrawGizmos()
     {
-        
+        player = GameObject.Find("Player");
+
+        if (Vector3.Distance(this.gameObject.transform.position, player.transform.position) < RangoVisionCircular && perseguir)
+        {
+            Gizmos.color = Color.red;
+        }
+        else { Gizmos.color = Color.green; }
         // Draw a yellow sphere at the transform's position
         if (DibujarEsfera)
         {
-            if (Vector3.Distance(this.gameObject.transform.position, player.transform.position) < RangoVisionCircular)
-            {
-                Gizmos.color = Color.red;
-            }
-            else
-            {
-                Gizmos.color = Color.green;
-            }
-            Gizmos.DrawWireSphere(this.gameObject.transform.position, RangoVisionCircular);
+        Gizmos.DrawWireSphere(this.gameObject.transform.position, RangoVisionCircular);
         }
+        Gizmos.color = Color.green;
+
     }
 }
