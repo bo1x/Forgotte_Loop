7aa6dd12e98081b9cd28a54ca029ea659118c15d
Bo1x <alejandroboixperez@gmail.com>
2023-03-09 18:57:03 +0100

    AI enemigo Melee Tonto

---
+++ Forgotten_Loop/Assets/Scripts/Alex/Ai/SimpleAI.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleAI : MonoBehaviour
+{
+    private GameObject player;
+    public float RangoVisionCircular = 5f;
+    public bool DibujarEsfera = true;
+    public bool perseguir = true;
+    public float velocidadMovimiento = 5f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //Obtienes distancia entre 2 puntos
+        //Debug.Log(Vector3.Distance(this.gameObject.transform.position, player.transform.position));
+        if (Vector3.Distance(this.gameObject.transform.position, player.transform.position) < RangoVisionCircular && perseguir)
+        {
+            this.transform.position = Vector3.MoveTowards(this.transform.position, player.transform.position, Time.deltaTime* velocidadMovimiento);
+        }
+
+
+    }
+
+    void OnDrawGizmos()
+    {
+        
+        // Draw a yellow sphere at the transform's position
+        if (DibujarEsfera)
+        {
+            if (Vector3.Distance(this.gameObject.transform.position, player.transform.position) < RangoVisionCircular)
+            {
+                Gizmos.color = Color.red;
+            }
+            else
+            {
+                Gizmos.color = Color.green;
+            }
+            Gizmos.DrawWireSphere(this.gameObject.transform.position, RangoVisionCircular);
+        }
+    }
+}
