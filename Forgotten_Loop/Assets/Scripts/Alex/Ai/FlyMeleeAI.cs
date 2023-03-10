7a88d3f58aa5c1df3fccfdf3c2c5453ae917578a
Bo1x <alejandroboixperez@gmail.com>
2023-03-10 22:36:55 +0100

    EnemigoVoladorConCarga

--- Forgotten_Loop/Assets/Scripts/Alex/Ai/FlyMeleeAI.cs
+++ Forgotten_Loop/Assets/Scripts/Alex/Ai/FlyMeleeAI.cs
@@ -7,12 +7,17 @@ public class FlyMeleeAI : MonoBehaviour
     private GameObject player;
     public float RangoVisionCircular = 5f;
     public float RangoParaAtaque = 2f;
-    public float TiempoCargaAtaque = 0.5f;
     public bool DibujarEsfera = true;
     public bool perseguir = true;
     public float velocidadMovimiento = 15f;
+    public float velocidadAtaqueCarga = 40f;
     private bool atacando = false;
+    private bool direccionPillada = false;
+    private Vector2 dir;
     private float tiempo = 0f;
+    public float TiempoCargaAtaque = 1f;
+    private float tiempoCargando = 0f;
+    public float tiempoEnLineaRecta = 1f;
 
     void Awake()
     {
@@ -30,12 +35,33 @@ public class FlyMeleeAI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Debug.Log(direccionPillada);
         if (atacando)
         {
             tiempo = tiempo + Time.deltaTime;
             if (tiempo > TiempoCargaAtaque)
             {
-                //Carga Ataque en vector direccion hacia el jugador
+                if (!direccionPillada)
+                {
+                  //  dir = (this.transform.position - player.transform.position).normalized;
+                    dir = (player.transform.position - this.transform.position).normalized;
+                    direccionPillada = true;
+                }
+                else
+                {
+
+                    transform.Translate(dir * velocidadAtaqueCarga * Time.deltaTime);
+                    tiempoCargando = tiempoCargando + Time.deltaTime;
+                }
+                
+                if(tiempoCargando> tiempoEnLineaRecta)
+                {
+                    atacando = false;
+                    direccionPillada = false;
+                    tiempo = 0f;
+                    tiempoCargando = 0f;
+                }
+                
             }
         }
         else if (Vector3.Distance(this.gameObject.transform.position, player.transform.position) < RangoVisionCircular && perseguir)
