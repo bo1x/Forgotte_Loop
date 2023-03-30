8fab06fa0aa5e93d847fe51a668314e470986b6f
Laura <exyr21@gmail.com>
2023-03-30 17:05:19 +0200

    armitas facheritas

--- Forgotten_Loop/Assets/Scripts/Shoot.cs
+++ Forgotten_Loop/Assets/Scripts/Shoot.cs
@@ -7,12 +7,24 @@ public class Shoot : MonoBehaviour
 {
     public Transform shootingPoint;
     public GameObject bulletPrefab;
+    public bool ModoDisparo = false;
+    public float tiempoD = 0;
+    public float cadencia = 0.5f;
 
     
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetButtonDown("Fire1") && ModoDisparo == true && Time.time > tiempoD)
         {
+            tiempoD = Time.time + cadencia;
+            GameObject myBullet = Instantiate(bulletPrefab, shootingPoint.position, transform.rotation);
+            Destroy(myBullet, 2f);
+        }
+
+        if (Input.GetButton("Fire1") && ModoDisparo == false && Time.time > tiempoD)
+            
+        {
+            tiempoD = Time.time + cadencia;
             GameObject myBullet = Instantiate(bulletPrefab, shootingPoint.position, transform.rotation);
             Destroy(myBullet, 2f);
         }
