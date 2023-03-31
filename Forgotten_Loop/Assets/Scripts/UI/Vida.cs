b3ffebb39c4960743b863a2e7030bf08dfca02b6
Laura <exyr21@gmail.com>
2023-04-01 00:27:06 +0200

    coso ui

---
+++ Forgotten_Loop/Assets/Scripts/UI/Vida.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vida : MonoBehaviour
+{
+    public GameObject corazon1;
+    public GameObject corazon2;
+    public GameObject corazon3;
+
+    public int vida = 3;
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        if (vida == 3)
+        {
+            corazon1.SetActive(true);
+            corazon2.SetActive(true);
+            corazon3.SetActive(true);
+        }
+
+        if (vida == 2)
+        {
+            corazon1.SetActive(true);
+            corazon2.SetActive(true);
+            corazon3.SetActive(false);
+        }
+
+        if (vida == 1)
+        {
+            corazon1.SetActive(true);
+            corazon2.SetActive(false);
+            corazon3.SetActive(false);
+        }
+        if (vida == 0)
+        {
+            corazon1.SetActive(false);
+            corazon2.SetActive(false);
+            corazon3.SetActive(false);
+        }
+    }
+}
