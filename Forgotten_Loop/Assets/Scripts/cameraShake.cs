4cb592172638683dc6290d163594b988b3932884
bo1x <alejandroboixperez@gmail.com>
2023-04-21 19:06:03 +0200

    z

--- Forgotten_Loop/Assets/Scripts/cameraShake.cs
+++ Forgotten_Loop/Assets/Scripts/cameraShake.cs
@@ -6,20 +6,37 @@ public class cameraShake : MonoBehaviour
 {
 
     private CinemachineVirtualCamera virtualCam;
+    private GameObject cam;
     private CinemachineBasicMultiChannelPerlin cameranoise;
+    float tiempo = 0;
+
+    public float amplitude = 5f;
+    public float freq = 5f;
     // Start is called before the first frame update
     void Start()
     {
+        cam = GameObject.Find("Main Camera");
         virtualCam = GetComponent<CinemachineVirtualCamera>();
         cameranoise = virtualCam.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
     }
 
+    private void Update()
+    {
+        tiempo = tiempo + Time.deltaTime;
+        
+        if (tiempo>1f)
+        {
+            Debug.Log("A");
+            Shake(amplitude, freq, 1f);
+            tiempo = 0f;
+        }
+    }
     public void Shake(float amplitude, float frequencyGain,float time)
     {
         cameranoise.m_AmplitudeGain = amplitude;
         cameranoise.m_FrequencyGain = frequencyGain;
 
-        CancelInvoke();
+        //CancelInvoke();
         Invoke("StopShake", time);
     }
 
@@ -27,7 +44,10 @@ public class cameraShake : MonoBehaviour
     {
 
         //Poner Z rot camara en 0 
+        //cam.transform.Rotate(0f, 0.0f, 0.0f, Space.Self);
         cameranoise.m_AmplitudeGain = 0;
         cameranoise.m_FrequencyGain = 0;
+
+        //cam.transform.rotation = Quaternion.Euler(0, 0, 0);
     }
 }
