5f9015b66bfd1297c81d2026347f857c1a335f71
Laura <exyr21@gmail.com>
2023-05-06 21:15:11 +0200

    cambio vel dash a la mitad. cambio cd dash a 4s.animacion UIDash

---
+++ Forgotten_Loop/Assets/Scripts/UI/CDDash.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class CDDash : MonoBehaviour
+{
+    
+    public GameObject cdDash;
+    private float contador;
+    public bool heDasheado = false;
+    public bool contando = false;
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+      heDasheado = GameObject.Find("Player").GetComponent<Control>().IsDashing;
+
+        Debug.Log(contador);
+        Debug.Log(contando);
+        if (heDasheado == true)
+        {
+            contando = true;            
+        }
+
+        if (contando)
+        {
+            contador = contador + Time.deltaTime;
+            cdDash.GetComponent<Image>().fillAmount = contador / 4f;
+        }
+        if (contador >= 4.0f)
+        {
+            Debug.Log("fin");
+           // contador = 0.0f;
+            contando = false;
+            
+        }
+        if (!contando)
+        {
+            contador = 0f;
+            cdDash.GetComponent<Image>().fillAmount = 1.0f;
+        }
+
+        }
+       
+    }
